640dc0f0ce0b362e2a020671fc23cd19d726496b
Babuprasanth R <babuprasanth.r2727@gmail.com>
2025-04-29 11:46:46 +0530

    change5

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import Hero from './components/Hero'
-import Works from './components/works'
+import Works from './components/Works'
 import AboutUs from './components/AboutUs'
 import Testimonial from './components/Testimonial'
 import WhyChooseUs from './components/WhyChooseUs'
